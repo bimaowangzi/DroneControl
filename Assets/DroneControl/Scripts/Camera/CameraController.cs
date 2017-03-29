ef5441727587848f22522f1513ca0faa1127473f
Lin Junsheng <junsheng_lin@mymail.sutd.edu.sg>
2017-03-29 18:48:55 +0800

    First commit of Unity Simulation. Control done for mobile device so far

---
+++ Assets/DroneControl/Scripts/Camera/CameraController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    [SerializeField]
+    private Transform target;
+
+    [SerializeField]
+    private Vector3 offsetPosition;
+
+    [SerializeField]
+    private Space offsetPositionSpace = Space.Self;
+
+    [SerializeField]
+    private bool lookAt = true;
+
+    private void Update()
+    {
+        Refresh();
+    }
+
+    public void Refresh()
+    {
+        if (target == null)
+        {
+            Debug.LogWarning("Missing target ref !", this);
+
+            return;
+        }
+
+        // compute position
+        if (offsetPositionSpace == Space.Self)
+        {
+            transform.position = target.TransformPoint(offsetPosition);
+        }
+        else
+        {
+            transform.position = target.position + offsetPosition;
+        }
+
+        Debug.Log("Before target: " + target.transform.localEulerAngles + ", Before self: " + transform.localEulerAngles);
+
+        // compute rotation
+        if (lookAt)
+        {
+            transform.LookAt(target);
+        }
+        else
+        {
+            transform.rotation = target.rotation;
+        }
+
+        Debug.Log("After target: " + target.transform.localEulerAngles + ", After self: " + transform.localEulerAngles);
+    }
+
+ //   public GameObject player;
+ //   private Vector3 offset;
+
+	//// Use this for initialization
+	//void Start () {
+ //       offset = transform.position - player.transform.position;
+	//}
+	
+	//// Update is called once per frame
+	//void LateUpdate () {
+ //       transform.position = player.transform.position + offset;
+	//}
+}
--- Assets/DroneControl/Scripts/Controller/Controller.cs
+++ Assets/DroneControl/Scripts/Controller/Controller.cs
@@ -1,36 +1,126 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
-public class Controller : MonoBehaviour {
+public class Controller : MonoBehaviour
+{
 
-	public float Throttle = 0.0f;
-	public float Yaw = 0.0f;
-	public float Pitch = 0.0f;
-	public float Roll = 0.0f;
+    public float Throttle = 0.0f;
+    public float Yaw = 0.0f;
+    public float Pitch = 0.0f;
+    public float Roll = 0.0f;
 
-    public enum ThrottleMode { None, LockHeight};
+    public Text debugText;
+    public Text valuesText;
 
-	[Header("Throttle command")]
-	public string ThrottleCommand = "Throttle";
-	public bool InvertThrottle = true;
+    public float touchSensitivity;
+    public float pitchSensitivity;
+    public float rollSensitivity;
+    private Vector2 pointerStart;
+
+    // accelStart are intialized to impossible values since X and Y are between -1 and 1
+    private float accelXStart = -2;
+    private float accelYStart = -2;
+    private float accelXdf = 0;
+    private float accelYdf = 0;
+
+    public enum ThrottleMode { None, LockHeight };
+
+    [Header("Throttle command")]
+    public string ThrottleCommand = "Throttle";
+    public bool InvertThrottle = true;
 
     [Header("Yaw Command")]
-	public string YawCommand = "Yaw";
-	public bool InvertYaw = false;
+    public string YawCommand = "Yaw";
+    public bool InvertYaw = false;
 
-	[Header("Pitch Command")]
-	public string PitchCommand = "Pitch";
-	public bool InvertPitch = true;
+    [Header("Pitch Command")]
+    public string PitchCommand = "Pitch";
+    public bool InvertPitch = true;
 
-	[Header("Roll Command")]
-	public string RollCommand = "Roll";
-	public bool InvertRoll = true;
+    [Header("Roll Command")]
+    public string RollCommand = "Roll";
+    public bool InvertRoll = true;
 
-	void Update() {
-        Throttle = Input.GetAxisRaw(ThrottleCommand) * (InvertThrottle ? -1 : 1);
-        Yaw = Input.GetAxisRaw(YawCommand) * (InvertYaw ? -1 : 1);
-        Pitch = Input.GetAxisRaw(PitchCommand) * (InvertPitch ? -1 : 1);
-        Roll = Input.GetAxisRaw(RollCommand) * (InvertRoll ? -1 : 1);
-	}
+    private void Start()
+    {
+        Screen.orientation = ScreenOrientation.LandscapeLeft;
+    }
+
+    void Update()
+    {
+
+        if (accelXStart == -2 || accelYStart == -2)
+        {
+            accelXStart = Input.acceleration.x;
+            accelYStart = Input.acceleration.y;
+        }
+
+        accelXdf = (Input.acceleration.x - accelXStart) * pitchSensitivity;
+        accelYdf = (Input.acceleration.y - accelYStart) * rollSensitivity;
+
+        //valuesText.text = string.Format("accelX: {0}\naccelY: {1}",
+        //                  accelXdf, accelYdf);
+
+        //Pitch = (accelXdf) * (InvertPitch ? -1 : 1);
+        //Roll = (accelYdf) * (InvertRoll ? -1 : 1);
+
+        if (Input.touchCount != 0)
+        {
+            //debugText.text = Input.touchCount + " touches detected";
+
+            //To get the finger id
+            //Input.touches[0].fingerId
+
+            Touch pointer1 = Input.touches[0];
+            if (pointer1.phase == TouchPhase.Began)
+            {
+                debugText.text = "Touch began";
+
+                pointerStart = Input.touches[0].position;
+            }
+            else if (pointer1.phase == TouchPhase.Moved)
+            {
+                debugText.text = "Touch moved";
+
+                Vector2 pointerCurrent = Input.touches[0].position;
+                Vector2 pointerDF = (pointerCurrent - pointerStart);
+
+                //float Xdf = pointerDF.x;
+                //float Ydf = pointerDF.y;
+
+                //valuesText.text = string.Format("Xdf: {0}\nYdf: {1}",
+                //          Xdf, Ydf);
+
+                pointerDF.Normalize();
+
+                float pointerXdf = pointerDF.x * touchSensitivity;
+                float pointerYdf = pointerDF.y * touchSensitivity;
+
+                valuesText.text = string.Format("xdf: {0}\nydf: {1}",
+                          pointerXdf, pointerYdf);
+
+                Throttle = (pointerYdf) * (InvertThrottle ? -1 : 1);
+                Yaw = (pointerXdf) * (InvertYaw ? -1 : 1);
+            }
+        }
+        else
+        {
+            if (Input.GetMouseButton(0))
+            {
+                // Whatever you want it to do.
+                debugText.text = "Mouse down detected";
+            }
+            else
+            {
+                debugText.text = "No touches";
+            }
+        }
+
+        //Throttle = Input.GetAxisRaw(ThrottleCommand) * (InvertThrottle ? -1 : 1);
+        //Yaw = Input.GetAxisRaw(YawCommand) * (InvertYaw ? -1 : 1);
+        //Pitch = Input.GetAxisRaw(PitchCommand) * (InvertPitch ? -1 : 1);
+        //Roll = Input.GetAxisRaw(RollCommand) * (InvertRoll ? -1 : 1);
+    }
 
 }
